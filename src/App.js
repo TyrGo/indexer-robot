e69deaccbecda7c07fcefe60c5f7fb08d05763d4
Ty <tyron.golds@gmail.com>
2021-02-04 22:07:27 -0500

    get key from env

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import NavBar from "./NavBar"
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import Thanks from "./Thanks.js"
-import PUB_KEY from "./config.js"
-// const PUB_KEY = process.env.REACT_APP_PUB_KEY;
+// import PUB_KEY from "./config.js"
+const PUB_KEY = process.env.REACT_APP_PUB_KEY;
 
 const stripePromise = loadStripe(PUB_KEY);
 
